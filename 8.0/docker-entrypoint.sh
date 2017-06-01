e091331205258dd9a2d44792c7f51b8b15df753b
Shantanu Oak <shantanu.oak@gmail.com>
2017-06-01 10:39:42 +0530

    increased wait timeout from 30 to 130

--- 8.0/docker-entrypoint.sh
+++ 8.0/docker-entrypoint.sh
@@ -104,7 +104,7 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 
 		mysql=( mysql --protocol=socket -uroot -hlocalhost --socket="${SOCKET}" )
 
-		for i in {30..0}; do
+		for i in {130..0}; do
 			if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
 				break
 			fi
